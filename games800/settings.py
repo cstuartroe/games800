900628e5d516dd498e5c31b4a9e889d6262415fd
Conor Stuart Roe <cstuartroe@haverford.edu>
2021-06-08 03:10:10 +0000

    Add conorstuartroe subdomain

--- games800/settings.py
+++ games800/settings.py
@@ -1,5 +1,4 @@
 import os
-from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -7,6 +6,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'games800.herokuapp.com',
+    "games.conorstuartroe.com",
 ]
 
 DEBUG = bool(os.getenv('DEBUG'))
