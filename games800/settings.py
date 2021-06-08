552c6e7561e8e97e964ac3b6757da8aaeee77d77
Conor Stuart Roe <cstuartroe@haverford.edu>
2021-06-08 02:30:12 +0000

    Use postgres

--- games800/settings.py
+++ games800/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'games800.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("PG_DB"),
+        'USER': os.getenv("PG_USER"),
+        'PASSWORD': os.getenv("PG_PASSWORD"),
+        'HOST': os.getenv("PG_HOST"),
+        'PORT': '5432',
     }
 }
 
---
+++ pg_creds.sh
@@ -0,0 +1,4 @@
+export PG_HOST=$(heroku config:get PG_HOST)
+export PG_DB=$(heroku config:get PG_DB)
+export PG_USER=$(heroku config:get PG_USER)
+export PG_PASSWORD=$(heroku config:get PG_PASSWORD)
